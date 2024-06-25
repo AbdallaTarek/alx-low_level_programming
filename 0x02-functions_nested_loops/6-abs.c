202da3017d5743a4cd4e3924333056edaa25e821
AbdallaTarek <abdalla.taarek@gmail.com>
2024-06-25 04:40:46 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _abs - func to return abs of numbers
+ * @c: is the input to check
+ *
+ * Return: Absolute value of num or zero
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_num
+			;
+		abs_num = -1 * c;
+		return (abs_num);
+	}
+	return (c);
+}
