a28d9ab437c5ab6ca59e8f04dad60d296963e23d
AbdallaTarek <abdalla.taarek@gmail.com>
2024-07-12 11:47:36 -0700

    libmy.

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - func to return abs of numbers
+ * @c: is the input to check
+ *
+ * Description: func to return abs of numbers
+ * Return: Absolute value of num or zero
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_num
+			;
+		abs_num = -1 * c;
+		return (abs_num);
+	}
+	return (c);
+}
